bf13d3f3529c580c4f3bb249af8f7f0430622f1d
Julius <kirill.vackevich@gmail.com>
2020-07-07 22:07:03 +0300

    change style of parameters

--- arg_interface.py
+++ arg_interface.py
@@ -5,24 +5,27 @@ def create_interface():
     parser = argparse.ArgumentParser()
     parser._actions[0].help = 'Show parameters'
 
-    parser.add_argument('-f', '--file',
-                        help='Use your own file with urls',
-                        action='store',
-                        default='urls.list',
-                        metavar='file'
-                        )
+    parser.add_argument(
+        '-f', '--file',
+        help='Use your own file with urls',
+        action='store',
+        default='urls.list',
+        metavar='file',
+    )
 
-    parser.add_argument('-s', '--silent',
-                        help='silent mode',
-                        action='store_true'
-                        )
+    parser.add_argument(
+        '-s', '--silent',
+        help='silent mode',
+        action='store_true',
+    )
 
-    parser.add_argument('-d', '--directory',
-                        help='directory name for JSON files',
-                        action='store',
-                        default='.',
-                        metavar='dir'
-                        )
+    parser.add_argument(
+        '-d', '--directory',
+        help='directory name for JSON files',
+        action='store',
+        default='.',
+        metavar='dir',
+    )
 
     args = parser.parse_args()
     return args
